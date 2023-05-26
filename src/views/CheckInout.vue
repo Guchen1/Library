8925691b738658b18d5cacfe26a093eecc5efce5
David "SuperBart" Chen <Songze@live.cn>
2023-05-26 14:42:58 +0800

    fix no borrow book info

--- src/views/CheckInout.vue
+++ src/views/CheckInout.vue
@@ -14,11 +14,8 @@
         </template>
         <a-button :disabled="!allBorrowable" class="batch">Batch Check Out</a-button>
       </a-popover>
-      <a-button :disabled="!allReturnable" class="batch" @click="batchOp(2)"
-        >Batch Return</a-button
-      ><a-button :disabled="!allRenewable" class="batch" @click="batchOp(3)"
-        >Batch Renew</a-button
-      >
+      <a-button :disabled="!allReturnable" class="batch" @click="batchOp(2)">Batch Return</a-button
+      ><a-button :disabled="!allRenewable" class="batch" @click="batchOp(3)">Batch Renew</a-button>
       <a-popover
         placement="bottom"
         v-model:visible="visibleB"
@@ -54,81 +51,76 @@
 </template>
 
 <script setup lang="ts">
-import BookTable from "@/components/BookTable.vue";
-import { reactive, ref, computed, onMounted, watch } from "vue";
-import { useAxios } from "@/stores/axios";
-import BookSearch from "@/components/BookSearch.vue";
-import type {
-  BookDetail,
-  BookInfo,
-  BookResponse,
-  BorrowRecord,
-  BorrowResponse,
-} from "@/types/type";
-import { useClient } from "@/stores/client";
-import { message } from "ant-design-vue";
-import type { AxiosResponse } from "axios";
-import dayjs from "dayjs";
-import "ant-design-vue/es/message/style/css";
-const axios = useAxios().Axios;
-const client = useClient();
-const visibleA = ref(false);
-const visibleB = ref(false);
-const batchName = ref("");
-const paName = ref("");
-const paPass = ref("");
-const data = reactive<BookInfo[]>([]);
-const table = ref<typeof BookTable>();
+import BookTable from '@/components/BookTable.vue'
+import { reactive, ref, computed, onMounted, watch } from 'vue'
+import { useAxios } from '@/stores/axios'
+import BookSearch from '@/components/BookSearch.vue'
+import type { BookDetail, BookInfo, BookResponse, BorrowRecord, BorrowResponse } from '@/types/type'
+import { useClient } from '@/stores/client'
+import { message } from 'ant-design-vue'
+import type { AxiosResponse } from 'axios'
+import dayjs from 'dayjs'
+import 'ant-design-vue/es/message/style/css'
+const axios = useAxios().Axios
+const client = useClient()
+const visibleA = ref(false)
+const visibleB = ref(false)
+const batchName = ref('')
+const paName = ref('')
+const paPass = ref('')
+const data = reactive<BookInfo[]>([])
+const table = ref<typeof BookTable>()
 onMounted(() => {
-  search("", "", "", "");
-});
+  search('', '', '', '')
+})
 const search = async (name: string, author: string, isbn: string, borrower: string) => {
   //TODO-C: Finish search function
-  data.splice(0, data.length);
-  let page: number = 1;
-  let isMore: boolean = true;
-  let bookBorrowInfo: BorrowRecord[] = [];
-  let bookInfo: BookDetail[] = [];
+  data.splice(0, data.length)
+  let page: number = 1
+  let isMore: boolean = true
+  let bookBorrowInfo: BorrowRecord[] = []
+  let bookInfo: BookDetail[] = []
   await axios
-    .post("/StaffOp/bookAllBorrowInfo", {
+    .post('/StaffOp/bookAllBorrowInfo', {
       opUser: client.clientData.clientName,
-      page: "1",
-      num: "999",
+      page: '1',
+      num: '999'
     })
     .then((e: AxiosResponse<BorrowResponse>) => {
       // Do not ask me why...
-      e.data.infoList.forEach((e: BorrowRecord) => {
-        if (
-          e.bookName.indexOf(name) != -1 &&
-          e.bookAuthor.indexOf(author) != -1 &&
-          e.bookIsbn.indexOf(isbn) != -1 &&
-          e.borrowAccount.indexOf(borrower) != -1
-        ) {
-          bookBorrowInfo.push(e);
-        }
-      });
+      if (e.data.infoList != null)
+        e.data.infoList.forEach((e: BorrowRecord) => {
+          if (
+            e.bookName.indexOf(name) != -1 &&
+            e.bookAuthor.indexOf(author) != -1 &&
+            e.bookIsbn.indexOf(isbn) != -1 &&
+            e.borrowAccount.indexOf(borrower) != -1
+          ) {
+            bookBorrowInfo.push(e)
+          }
+        })
     })
     .catch((e: any) => {
-      message.error(e);
-    });
+      message.error('search borrowAllInfo' + e)
+    })
   try {
     while (isMore) {
       await axios
-        .post("/UserOp/searchBook", {
+        .post('/UserOp/searchBook', {
           opUser: client.clientData.clientName,
-          name: "",
-          isbn: "",
-          author: "",
+          name: '',
+          isbn: '',
+          author: '',
           page: String(page),
-          ready: true,
+          ready: true
         })
         .then((res: AxiosResponse<BookResponse>) => {
           if (!res.status) {
-            isMore = false;
-            throw "Unable to get data with status code " + res.status;
+            isMore = false
+            throw 'Unable to get data with status code ' + res.status
           }
           if (res.data.books.length == 0) {
-            isMore = false;
+            isMore = false
           } else {
             res.data.books.forEach((e: BookDetail) => {
               if (
@@ -136,24 +128,24 @@ const search = async (name: string, author: string, isbn: string, borrower: stri
                 e.bookAuthor.indexOf(author) != -1 &&
                 e.bookIsbn.indexOf(isbn) != -1
               ) {
-                bookInfo.push(e);
+                bookInfo.push(e)
               }
-            });
-            page += 1;
+            })
+            page += 1
           }
         })
         .catch((e: any) => {
-          isMore = false;
-          throw e;
-        });
+          isMore = false
+          throw e
+        })
     }
   } catch (err: any) {
-    message.error(err);
+    message.error('searchbook isMore' + err)
   }
 
   bookInfo.forEach((e: any) => {
-    let whatever = e;
-    let count: number = 0;
+    let whatever = e
+    let count: number = 0
     function a(whatever: any) {
       return (e: BorrowRecord) => {
         if (e.bookName == whatever.bookName) {
@@ -163,25 +155,21 @@ const search = async (name: string, author: string, isbn: string, borrower: stri
             isbn: whatever.bookIsbn,
             author: whatever.bookAuthor,
             borrower: e.borrowAccount,
-            borrowdate: dayjs(e.borrowTime, "YYYY-MM-DD"),
-            duedate: dayjs(e.borrowTime, "YYYY-MM-DD").add(e.borrowDuration, "day"),
-            returndate: dayjs(e.borrowTime, "YYYY-MM-DD").add(e.borrowDuration, "day"),
+            borrowdate: dayjs(e.borrowTime, 'YYYY-MM-DD'),
+            duedate: dayjs(e.borrowTime, 'YYYY-MM-DD').add(e.borrowDuration, 'day'),
+            returndate: dayjs(e.borrowTime, 'YYYY-MM-DD').add(e.borrowDuration, 'day'),
             status:
-              e.borrowIsOverTime == 1
-                ? "overdue"
-                : e.borrowIsrenew == 1
-                ? "renewed"
-                : "borrowed",
+              e.borrowIsOverTime == 1 ? 'overdue' : e.borrowIsrenew == 1 ? 'renewed' : 'borrowed',
             renewable: e.borrowIsrenew == 0,
             visible: true,
-            borrowId: e.borrowId,
-          });
-          count++;
+            borrowId: e.borrowId
+          })
+          count++
         }
-      };
+      }
     }
-    bookBorrowInfo.forEach(a(whatever));
-    if (!(borrower != "" && e.borrowAccount == undefined)) {
+    bookBorrowInfo.forEach(a(whatever))
+    if (!(borrower != '' && e.borrowAccount == undefined)) {
       for (var i = 0; i < whatever.bookStock - count; ++i) {
         data.push({
           name: whatever.bookName,
@@ -192,120 +180,120 @@ const search = async (name: string, author: string, isbn: string, borrower: stri
           borrowdate: undefined,
           duedate: undefined,
           returndate: e.returndate,
-          status: "available",
+          status: 'available',
           renewable: undefined,
           visible: false,
-          borrowId: undefined,
-        });
+          borrowId: undefined
+        })
       }
     }
-  });
-};
+  })
+}
 //TODO-C: Create Pateron
 const newPateron = async () => {
   if (paName.value.length == 0 || paPass.value.length == 0) {
-    message.error("Pleace fulfill all info.");
+    message.error('Pleace fulfill all info.')
   } else {
     await axios
-      .post("/ManagerOp/creatRole", {
+      .post('/ManagerOp/creatRole', {
         opUser: client.clientData.clientName,
         account: paName.value,
         password: paPass.value,
-        type: "user",
-        email: "default@example.com",
+        type: 'user',
+        email: 'default@example.com'
       })
       .then((res: any) => {
-        console.log(res);
+        console.log(res)
         if (!res.data.status) {
-          throw res.data.msg.content;
+          throw res.data.msg.content
         } else {
-          message.info(`Create pateron complete`);
+          message.info(`Create pateron complete`)
         }
       })
       .catch((err: any) => {
-        message.error(`Error detected while creating pateron: ${err}`);
-      });
+        message.error(`Error detected while creating pateron: ${err}`)
+      })
   }
-};
+}
 //TODO-C: Batch Op, Checkout Return Renew
 const batchOp = (optype: number) => {
   if (optype == 1) {
-    table.value?.checkList.forEach((e: any) => table.value?.borrow(e, batchName.value));
+    table.value?.checkList.forEach((e: any) => table.value?.borrow(e, batchName.value))
   } else if (optype == 2) {
-    table.value?.checkList.forEach((e: any) => table.value?.returnBook(e));
+    table.value?.checkList.forEach((e: any) => table.value?.returnBook(e))
   } else {
-    table.value?.checkList.forEach((e: any) => table.value?.renew(e));
+    table.value?.checkList.forEach((e: any) => table.value?.renew(e))
   }
-};
+}
 const props = defineProps<{
-  width: number;
-  height: number;
-}>();
+  width: number
+  height: number
+}>()
 const computedWidth = computed(() => {
   return {
-    first: props.width >= 1070 ? "0px" : "-14px",
-    second: props.width >= 1070 ? "0px" : "-10px",
-    third: props.width >= 1070 ? "0px" : "5px",
-  };
-});
+    first: props.width >= 1070 ? '0px' : '-14px',
+    second: props.width >= 1070 ? '0px' : '-10px',
+    third: props.width >= 1070 ? '0px' : '5px'
+  }
+})
 const allBorrowable = computed(() => {
   //遍历查看是否选中的均可借阅
   for (let i = 0; i < table.value?.currentList.length; i++) {
-    if (table.value?.currentList[i].status != "available") {
-      return false;
+    if (table.value?.currentList[i].status != 'available') {
+      return false
     }
   }
   if (table.value?.currentList.length == 0) {
-    return false;
+    return false
   }
-  return true;
-});
+  return true
+})
 const allRenewable = computed(() => {
   //遍历查看是否选中的均可续借
-  let res = true;
+  let res = true
   for (let i = 0; i < table.value?.currentList.length; i++) {
     if (!table.value?.currentList[i].renewable) {
-      res = false;
+      res = false
     }
   }
   if (table.value?.currentList.length == 0) {
-    res = false;
+    res = false
   }
-  return res;
-});
+  return res
+})
 const allReturnable = computed(() => {
   //遍历查看是否选中的均可归还
   for (let i = 0; i < table.value?.currentList.length; i++) {
     if (
-      table.value?.currentList[i].status != "borrowed" &&
-      table.value?.currentList[i].status != "overdue" &&
-      table.value?.currentList[i].status != "renewed"
+      table.value?.currentList[i].status != 'borrowed' &&
+      table.value?.currentList[i].status != 'overdue' &&
+      table.value?.currentList[i].status != 'renewed'
     ) {
-      return false;
+      return false
     }
   }
   if (table.value?.currentList.length == 0) {
-    return false;
+    return false
   }
-  return true;
-});
+  return true
+})
 const clearList = computed(() => {
-  return table?.value?.clearList;
-});
-onMounted(() => {});
+  return table?.value?.clearList
+})
+onMounted(() => {})
 defineExpose({
-  clearList,
-});
+  clearList
+})
 watch(visibleA, () => {
-  batchName.value = "";
-  paName.value = "";
-  paPass.value = "";
-});
+  batchName.value = ''
+  paName.value = ''
+  paPass.value = ''
+})
 watch(visibleB, () => {
-  batchName.value = "";
-  paName.value = "";
-  paPass.value = "";
-});
+  batchName.value = ''
+  paName.value = ''
+  paPass.value = ''
+})
 </script>
 
 <style scoped>
@@ -326,12 +314,12 @@ watch(visibleB, () => {
   font-size: 30px;
   font-weight: bold;
   display: inline-flex;
-  margin-top: v-bind("computedWidth.first");
+  margin-top: v-bind('computedWidth.first');
 }
 
 .item {
-  margin-top: v-bind("computedWidth.second");
-  margin-bottom: v-bind("computedWidth.third");
+  margin-top: v-bind('computedWidth.second');
+  margin-bottom: v-bind('computedWidth.third');
 }
 
 @media screen and (max-width: 872px) {
