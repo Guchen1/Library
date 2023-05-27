a04cbb0afc29c95b00724048ab71dc8716f1681c
guchen <1023797278@qq.com>
2023-05-27 19:27:36 +0800

    Update url

--- src/stores/axios.ts
+++ src/stores/axios.ts
@@ -6,7 +6,7 @@ export const useAxios = defineStore('axios', () => {
   const Axios = ref()
   const serverUrl = ref('https://library.superbart.xyz')
   Axios.value = axios.create({
-    baseURL: 'https://server.superbart.xyz:8433'
+    baseURL: 'https://spm.guchen.work:8433'
     // withCredentials:true,
   })
 
