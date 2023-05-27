1bf5945d3ea75eb332ee8b5e1f3ca2ee5b208b6f
guchen <1023797278@qq.com>
2023-05-27 14:41:41 +0800

    Alter url

--- src/stores/axios.ts
+++ src/stores/axios.ts
@@ -4,7 +4,7 @@ import axios from 'axios'
 
 export const useAxios = defineStore('axios', () => {
   const Axios = ref()
-  const serverUrl = ref('http://localhost')
+  const serverUrl = ref('https://library.superbart.xyz')
   Axios.value = axios.create({
     baseURL: 'https://server.superbart.xyz:8433'
     // withCredentials:true,
